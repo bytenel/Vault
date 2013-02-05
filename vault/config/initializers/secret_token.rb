1c954d399e0291e3d7551ea1623b5bc4060834af
Ben Nelson <ben@bytebot.(none)>
2013-02-04 18:07:25 -0700

    adding in scaffolding

---
+++ vault/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vault::Application.config.secret_token = '5885cf04c49a8eea95ce2772e775d3d79e6e9d2283dd41206dd7ba2635ae2a6f0a1ea83e9293212c8a56f2ca5819ac06339e55475e61409fd4e9df529506efd3'

fc93b3c95c7d983837aac6765284770f7d4c4f12
Ben Nelson <nelson.ben.c@gmail.com>
2013-03-18 07:45:23 -0600

    adding in stuff for simple recent activity and fixing some styling

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,5 @@
 class Document < ActiveRecord::Base
   belongs_to :user
   attr_accessible :name, :user
-  scope :recent, lambda { |user| where("created_at <= ? and user_id = ?", Time.zone.now, user.id) }
+  scope :recent, last(10)
 end

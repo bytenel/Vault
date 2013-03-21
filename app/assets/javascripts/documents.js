e3e557918f3061f05e89a64255e0357419e81a77
Ben Nelson <nelson.ben.c@gmail.com>
2013-03-20 23:29:20 -0600

    finished fixing jquery selector issues.

--- app/assets/javascripts/documents.js
+++ app/assets/javascripts/documents.js
@@ -1,8 +1,9 @@
 function toggle(name){ 
+	var past = $('#document_sections').find('button.active');
 	$('#document_sections').find('button').removeClass("active");
 	$('#document_sections').find('button.'+name).addClass("active");
 	$('#document_sections').find('button.'+name).addClass("started");
 	$('#document_sections').find('button.'+name).removeClass("not_started");
-	$('.content').toggle(); 
-	$('.content').find("#"+name).toggle(); 
+	$('.content').children('div').hide(); 
+	$('.content').find("."+name).toggle(); 
 	}

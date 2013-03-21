a4089e96c1697d012b250423c35257e100441371
Ben Nelson <nelson.ben.c@gmail.com>
2013-03-20 20:06:43 -0600

    fixing bug with active section jquery selection and class additions

--- app/assets/javascripts/documents.js
+++ app/assets/javascripts/documents.js
@@ -1,8 +1,8 @@
 function toggle(name){ 
-	$('.document_sections').find('button').removeClass("active");
-	$('.document_sections').find('button#'+name).addClass("active");
-	$('.document_sections').find('button#'+name).addClass("started");
-	$('.document_sections').find('button#'+name).removeClass("not_started");
+	$('#document_sections').find('button').removeClass("active");
+	$('#document_sections').find('button.'+name).addClass("active");
+	$('#document_sections').find('button.'+name).addClass("started");
+	$('#document_sections').find('button.'+name).removeClass("not_started");
 	$('.content').toggle(); 
 	$('.content').find("#"+name).toggle(); 
 	}
